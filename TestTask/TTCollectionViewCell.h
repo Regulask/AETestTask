713ee4961f78f89502c8324bdad6b145734329e0
Kasim Asyukov <kasimasyukov@Kasims-MacBook-Air.local>
2016-10-16 12:40:33 +0300

    added collection cell

---
+++ TestTask/TTCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TTCollectionViewCell.h
+//  TestTask
+//
+//  Created by Kasim Asyukov on 16/10/2016.
+//  Copyright © 2016 Kasim Asyukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@end
---
+++ TestTask/TTCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  TTCollectionViewCell.m
+//  TestTask
+//
+//  Created by Kasim Asyukov on 16/10/2016.
+//  Copyright © 2016 Kasim Asyukov. All rights reserved.
+//
+
+#import "TTCollectionViewCell.h"
+
+@implementation TTCollectionViewCell
+
+@end
