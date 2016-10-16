1a1cc3d1103e22711593683c86ea3b3df359086c
Kasim Asyukov <kasimasyukov@Kasims-MacBook-Air.local>
2016-10-16 11:37:38 +0300

    init

---
+++ TestTask/TestTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTask
+//
+//  Created by Kasim Asyukov on 16/10/2016.
+//  Copyright © 2016 Kasim Asyukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
